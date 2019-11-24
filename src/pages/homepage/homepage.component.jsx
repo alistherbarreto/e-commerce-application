6f8650d901b1ddcf660e3eb02dbb8ae468c692da
Alisther <alisther1627@gmail.com>
2019-11-24 19:06:28 +0530

    removed unwanted comments

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import './homepage.styles.scss'
-//import '../../pages/homepage/homepage.styles.scss';
-
-import Directory from '../../components/directory/directory.component'
 
 const HomePage = () => (
     <div className="homepage">
