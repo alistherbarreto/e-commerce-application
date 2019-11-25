b9287667c7dfc51e6760d2b470bc32ddc46de80b
alisther <alisther@opspl.com>
2019-11-25 14:55:29 +0530

    Creating Routes and wrapper methods (BrowserRouter/Switch/Route/withRouter/history/otherSectionProps)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
+
 import './App.css';
 import HomePage  from './pages/homepage/homepage.component'
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats</h1>
+  </div>
+);
+
+const ErrorPage = () => (
+  <div>
+    <h1>Invallid !</h1>
+  </div>
+);
 function App() {
   return (
     <div>
-      <HomePage>
-      </HomePage>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/shop/hats" component={HatsPage}/>
+        <Route exact path="*" component={ErrorPage}/>
+      </Switch>
     </div>
   );
 }
