67d64523aca7cf6f03bee6bc5e24fea4b8269e25
alisther <alisther@opspl.com>
2019-12-02 13:25:30 +0530

    added signin component,input component and button component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {Route, Switch} from 'react-router-dom'
 import './App.css';
 import HomePage  from './pages/homepage/homepage.component';
 import ShopPage  from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 
 const ErrorPage = () => (
@@ -15,9 +17,11 @@ const ErrorPage = () => (
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route exact path="/shop" component={ShopPage}/>
+        <Route exact path="/signin" component={SignInAndSignUpPage}/>
         <Route exact path="*" component={ErrorPage}/>
       </Switch>
     </div>
